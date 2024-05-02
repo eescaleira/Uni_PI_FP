c90a866859d2f61300db0cc4c8a22f8f8bdce103
escaleira <129979364+eescaleira@users.noreply.github.com>
2024-05-02 12:10:20 +0100

    menus 1 functions creation

--- src/menu_1_rmv_func.c
+++ src/menu_1_rmv_func.c
@@ -1,46 +1,41 @@
 #include "../inc/store_manager.h"
 
-// ai generated
+// ai generated help
 int rmv_store_name(){
 	char name[100];
+	t_store *temp = NULL;
 	printf("Enter the name of the store you want to remove: ");
 	scanf("%s", name);
-	if (store_name_exists(name)){// try to print store before removing and asking if sure
+	if ((temp = store_name_exists(name)) != NULL/* criar funcao para escolher algoritmo usado */){// try to print store before removing and asking if sure
 		system("clear");
-		remove_store_by_name(name);
+		remove_name(name);
 		printf("Store removed successfully!\n");
-		system("clear");
-		menu_1_handle();
 	}
 	else{
 		system("clear");
 		printf("Store with name %s does not exist!\n", name);
-		sleep(3);
-		system("clear");
-		menu_1_handle();		
 	}
+	sleep(3);
+	system("clear");
+	menu_1_handle();
 	return 0;
 }
-// ai generated
+// ai generated help
 int rmv_store_id(){
 	int id;
+	t_store *temp = NULL;
 	printf("Enter the id of the store you want to remove: ");
 	scanf("%di", &id);
-	if (store_id_exists(id)/* criar funcao para escolher algoritmo usado */){// try to print store before removing and asking if sure
+	if ((temp = store_id_exists(id)) != NULL/* criar funcao para escolher algoritmo usado */){// try to print store before removing and asking if sure
 		system("clear");
-		remove_store_by_id(id);
 		printf("Store removed successfully!\n");
-		system("clear");
-		menu_1_handle();
 	}
 	else{
 		system("clear");
 		printf("Store with id %d does not exist!\n", id);
-		sleep(3);
-		system("clear");
-		menu_1_handle();		
 	}
+	sleep(3);
+	system("clear");
+	menu_1_handle();
 	return 0;
 }
-
-
